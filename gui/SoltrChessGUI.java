b454158ce44eef7ce03fdd9a918218564f40e06a
Phuvit Kittisapkajon <pxk8682@rit.edu>
2016-11-18 13:12:19 -0500

    initial version:1

---
+++ gui/SoltrChessGUI.java
@@ -0,0 +1,25 @@
+/**
+ * Part of SoltrChessLayout project.
+ * Created 10 2015
+ *
+ * @author James Heliotis
+ */
+
+package gui;
+
+import javafx.application.Application;
+
+/**
+ * A miniature chess board
+ *
+ * @author James Heliotis
+ */
+public class SoltrChessGUI extends Application {
+    /**
+     * Construct the layout for the game.
+     *
+     * @param stage container (window) in which to render the UI
+     */
+    public void start( Stage stage ) {
+    }
+}
---
+++ ptui/SoltrChessPTUI.java
@@ -0,0 +1,30 @@
+package ptui;
+
+import model.*;
+
+import static model.SoltrChessModel.DIMENSION;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * Part of SoltrChess project.
+ * Created 11 2015
+ *
+ * @author James Heliotis
+ */
+public class SoltrChessPTUI {
+
+    public SoltrChessPTUI( String fileName ) {
+    }
+
+    // CONTROLLER
+    public void run() {
+    }
+
+    // VIEW
+}
